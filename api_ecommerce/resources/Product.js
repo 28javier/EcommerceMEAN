c85c1520581a783975e5e263866fdc6298fe8b86
28javier <javier19952009@gmail.com>
2023-07-29 22:32:17 -0500

    primer commit del proyecto

---
+++ api_ecommerce/resources/Product.js
@@ -0,0 +1,34 @@
+export default {
+    product_list: (product,variedades = []) => {
+        // var IMAGEN_TWO = "";
+        
+        // var VAL = Math.floor(Math.random() * 3);//0,1,2
+        // IMAGEN_TWO = GALERIAS[VAL].imagen;
+        return {
+            _id: product._id,
+            title: product.title,
+            sku: product.sku,
+            slug: product.slug,
+            imagen: process.env.URL_BACKEND+'/api/products/uploads/product/'+product.portada,//*
+            categorie: product.categorie,
+            price_soles: product.price_soles,
+            price_usd: product.price_usd,
+            stock: product.stock,
+            description: product.description,
+            resumen: product.resumen,
+            tags: product.tags ? JSON.parse(product.tags) : [],
+            type_inventario: product.type_inventario,
+            state: product.state,
+            variedades: variedades,
+            galerias:product.galerias.map((galeria) => {
+                galeria.imagen = process.env.URL_BACKEND+'/api/products/uploads/product/'+galeria.imagen;//*
+                return galeria;
+            })
+            // imagen_two: IMAGEN_TWO,
+            // galerias:GALERIAS,
+            // avg_review: avg_review,
+            // count_review: count_review,
+            // campaing_discount: CampaingDiscount,
+        }
+    }
+}
